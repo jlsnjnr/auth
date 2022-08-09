0f465bf284f3662365d0140195b049b67b614fa8
Joelson Júnior <70169714+jlsnjnr@users.noreply.github.com>
2022-08-09 18:46:05 -0300

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,35 @@
+import Head from 'next/head';
+import { FormEvent, useContext, useState } from 'react';
+import { AuthContext } from '../contexts/AuthContext';
+
+export default function Home() {
+  const { signIn } = useContext(AuthContext);
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  async function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+    const data = {
+      email,
+      password,
+    }
+
+    await signIn(data);
+  }
+
+  return (
+    <>
+      <Head>
+        <title>Auth</title>
+      </Head>
+      <div className="container">
+        <form onSubmit={handleSubmit}>
+          <input placeholder="Email" type="email" value={email} onChange={e => setEmail(e.target.value)} />
+          <input placeholder="Senha" type="value" value={password} onChange={e => setPassword(e.target.value)} />
+          <button type="submit">Entrar</button>
+        </form>
+      </div>
+    </>
+  )
+}
